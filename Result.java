e9249375586bd15bdd4d3da4ebef33437e0ee490
AmuthaShunmugam <45200546+AmuthaShunmugam@users.noreply.github.com>
2018-11-21 13:52:17 +0530

    Add files via upload

---
+++ Result.java
@@ -0,0 +1,16 @@
+class Result
+{
+public static void main(String[] args)
+{
+int a,b,c,d,e;
+a = 20;
+b = 10;
+b = a-- - --a;
+c = a--;
+d = a >> 2;
+e = a & b;
+System.out.println ("Value of b is :" + b);
+System.out.println ("Value of c is :" + c);
+System.out.println ("Value of d is :" + d);
+System.out.println ("Value of e is :" + e);
+}}
